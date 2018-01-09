94fb3fdf60bf4ba6443a28fe8bcc268b91e3d480
yuefengh <e68info@yahoo.com.tw>
2018-01-09 11:42:21 +0800

    get a line message

--- receive.php
+++ receive.php
@@ -2,7 +2,7 @@
   $json_str = file_get_contents('php://input'); //接收 REQUEST的BODY
   $json_obj = json_decode($json_str); //轉JSON格式
 
-  $myfile = open("log.txt", "w+") or die("Unable to open file;"); //設定一個log.txt用來印訊息
-  fwrite($myfile, "\xEF\xBB\xBf".$json_str); //在字串前加入\xEF\xBB\xBf轉成utf8格式
+  $myfile = fopen("log.txt", "w+") or die("Unable to open file;"); //設定一個log.txt用來印訊息
+  fwrite($myfile, "\xEF\xBB\xBF".$json_str); //在字串前加入\xEF\xBB\xBF轉成utf8格式
   fclose($myfile);
 ?>
