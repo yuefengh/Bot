09576faabf8b5d52fc8fd4417e99b5d4fef76b7f
yuefengh <e68info@yahoo.com.tw>
2018-01-09 11:36:19 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -1,3 +1,8 @@
 <?php
-  echo "hello i'm yfh"
+  $json_str = file_get_contents('php://input'); //接收 REQUEST的BODY
+  $json_obj = json_decode($json_str); //轉JSON格式
+
+  $myfile = open("log.txt", "w+") or die("Unable to open file;"); //設定一個log.txt用來印訊息
+  fwrite($myfile, "\xEF\xBB\xBf".$json_str); //在字串前加入\xEF\xBB\xBf轉成utf8格式
+  fclose($myfile);
 ?>
