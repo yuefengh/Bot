ac51840a87a18fcc53675f38fc7d0209be9ec215
yuefengh <e68info@yahoo.com.tw>
2018-01-09 14:51:19 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -44,6 +44,34 @@
 				)
 			);
         		break;
+		 case "location":
+			$line_server_url = 'https://api.line.me/v2/bot/message/reply';
+        		$response = array (
+				"replyToken" => $sender_replyToken,
+				"messages" => array (
+					array (
+						"type" => "location",
+						"title" => "my location",
+						"address" => "〒150-0002 東京都渋谷区渋谷２丁目２１−１",
+            					"latitude" => 35.65910807942215,
+						"longitude" => 139.70372892916203
+					)
+				)
+			);
+        		break;
+		case "sticker":
+			$line_server_url = 'https://api.line.me/v2/bot/message/reply';
+        		$response = array (
+				"replyToken" => $sender_replyToken,
+				"messages" => array (
+					array (
+						"type" => "sticker",
+						"packageId" => "1",
+						"stickerId" => "1"
+					)
+				)
+			);
+        		break;
  }
  $myfile = fopen("log.txt","w+") or die("Unable to open file!"); //設定一個log.txt 用來印訊息
  fwrite($myfile, "\xEF\xBB\xBF".json_encode($response)); //在字串前加入\xEF\xBB\xBF轉成utf8格式
